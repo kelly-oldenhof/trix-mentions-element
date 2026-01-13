4d7b27c8102622aa5640f4de84197f519db6d8a0
Kelly Oldenhof <kelly.oldenhof@paytools.com.au>
2026-01-13 11:21:49 +1100

    Add dist folder for direct GitHub installation

---
+++ dist/query.d.ts
@@ -0,0 +1,11 @@
+type Query = {
+    text: string;
+    position: number;
+};
+type QueryOptions = {
+    lookBackIndex: number;
+    multiWord: boolean;
+    lastMatchPosition: number | null;
+};
+export default function query(text: string, key: string, cursor: number, { multiWord, lookBackIndex, lastMatchPosition }?: QueryOptions): Query | void;
+export {};
---
+++ dist/trix-mentions-element.d.ts
@@ -0,0 +1,15 @@
+type Key = {
+    key: string;
+    multiWord: boolean;
+};
+export default class TrixMentionsElement extends HTMLElement {
+    get keys(): Key[];
+    get src(): string | null;
+    set src(value: string | null);
+    get name(): string | null;
+    set name(value: string | null);
+    connectedCallback(): void;
+    disconnectedCallback(): void;
+    dismiss(): void;
+}
+export {};
